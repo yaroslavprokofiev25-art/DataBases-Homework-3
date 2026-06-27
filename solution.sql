5c653cd6aad6086ee9de2b0c7207a6a532d36221
prkfv <yaroslav.prokofiev.25@kse.org.ua>
2026-06-27 17:54:33 +0300

    доробив всі 6 тасків

--- solution.sql
+++ solution.sql
@@ -1,3 +1,10 @@
+drop table if exists order_log;
+drop table if exists order_items;
+drop table if exists orders;
+drop table if exists products;
+drop table if exists customers;
+
+
 create table customers (
                            customer_id serial primary key,
                            full_name varchar(100) not null,
@@ -39,22 +46,22 @@ create table order_log (
 -- task 1
 create or replace function calculate_order_total(p_order_id int)
 returns numeric (10, 2)
+-- ця функція повертатиме загальну суму замовлень, але якщо воно порожнє то 0
 language plpgsql
 as $$
-declare
-    v_total numeric(10,2);
 begin
-    select coalesce(sum(quantity * price), 0)
-    into v_total
-    from order_items
-    where order_id = p_order_id;
-
-    return v_total;
+    return (
+        select coalesce(sum(quantity * price), 0)
+        from order_items
+        where order_id = p_order_id
+    );
 end;
 $$;
+
 --task 2
 create or replace procedure create_order(p_customer_id int)
 language plpgsql
+    -- ця процедура робить нове замовлення, але якщо клієнта не існує то нічого не зробить
 as $$
 begin
     insert into orders (customer_id, total_amount)
@@ -68,43 +75,102 @@ end;
 $$;
 
 --task 3
-create or replace procedure add_product_to_order(
+create or replace procedure add_product_to_order( -- ця процедура додає товар до замовлення і робить чек кількості
     p_order_id int,
     p_product_id int,
     p_quantity int
 )
 language plpgsql
 as $$
-declare
-    v_price numeric;
+
 begin
     if p_quantity <= 0 then
         raise exception 'Quantity must be positive';
     end if;
-
-    select price into v_price
-    from products
-    where product_id = p_product_id and stock_quantity >= p_quantity;
-
-    if not found then raise exception 'Product not found';
+    if not exists (
+        select 1 from products
+        where product_id = p_product_id and stock_quantity >= p_quantity
+    ) then
+        raise exception 'Product not found';
     end if;
-
     insert into order_items (order_id, product_id, quantity, price)
-    values (p_order_id, p_product_id, p_quantity, v_price);
-
+    select p_order_id, p_product_id, p_quantity, price
+    from products
+    where product_id = p_product_id;
     update products
     set stock_quantity = stock_quantity - p_quantity
     where product_id = p_product_id;
 end;
 $$;
 
--- task 4
-create or replace function trg_update_order_total()
-returns trigger
+-- task 4 тут тригер(чи тригерна функція) перераховує загальну суму ордера за допомогою task 1
+create or replace function trg_update_order_total() -- спочатку функція з логікою
+returns trigger -- ну це відпоівдно вказує що функція буде використовуватись як тригерна
+language plpgsql
 as $$
-declare v_order_int;
 begin
-    -- firslty determinate which order was affected
-
+    update orders
+    set total_amount = calculate_order_total(
+                       case when tg_op = 'DELETE' then old.order_id else new.order_id end
+                       )
+    where order_id = case when tg_op = 'DELETE' then old.order_id else new.order_id end;
+    return null;
 end;
-$$
+$$;
+
+drop trigger if exists update_order_total on order_items;
+create trigger update_order_total -- потім тригер який її викликає
+    -- він спрацьовує після якої-небудь зміни в order_items
+after insert or update or delete on order_items
+for each row execute function trg_update_order_total();
+
+
+--task 5
+create or replace function trg_log_new_order()
+returns trigger
+language plpgsql -- ця функція записує лог після створення замовлення
+as $$
+begin
+    insert into order_log (order_id, customer_id, action, log_date)
+    values (new.order_id, new.customer_id, 'Order_Created', current_timestamp);
+
+    return new;
+end;
+$$;
+
+drop trigger if exists log_new_order on orders;
+create trigger log_new_order
+after insert on orders -- і тут тригер спрацьовує після вставлення в таблицю з ордерами
+for each row execute function trg_log_new_order();
+
+
+--task 6
+
+truncate order_log, order_items, orders, products, customers restart identity cascade;
+
+-- create customers
+insert into customers (full_name, email)
+values ('Alice', 'alice@mail.com'),
+       ('Bob', 'bob@mail.com');
+
+-- create products
+insert into products (product_name, price, stock_quantity)
+values ('Laptop', 999.99, 10),
+       ('Mouse', 25.50, 50);
+
+-- створив ордери через процедуру
+call create_order(1);
+call create_order(2);
+
+-- додав товари через процедуру
+call add_product_to_order(1, 1, 2);
+call add_product_to_order(1, 2, 3);
+
+-- перевірив в ордері загальну кількість
+select order_id, total_amount from orders;
+
+-- чи правильно зменшився залишок
+select product_id, product_name, stock_quantity from products;
+
+-- і чек ордер логу
+select * from order_log;
